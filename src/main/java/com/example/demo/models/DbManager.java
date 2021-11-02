8ca4481bb14a60d811b42cd9f13bf97dfd57d1eb
Qvijae <puh18071976@mail.ru>
2021-11-02 18:36:08 +0300

    123

--- src/main/java/com/example/demo/models/DbManager.java
+++ src/main/java/com/example/demo/models/DbManager.java
@@ -14,7 +14,7 @@ public class DbManager {
         return instance;
     }
 
-    private String URL = "jdbc:postgresql://ec2-54-216-17-9.eu-west-1.compute.amazonaws.com:5432/de9rphrmehomn0?currentSchema=movies";
+    private String URL = "jdbc:postgresql://ec2-54-216-17-9.eu-west-1.compute.amazonaws.com:5432/de9rphrmehomn0";
     private String LOGIN = "pehunnoweicslu";
     private String PASSWORD = "a905330a9a8c3e3c119b408db1e210ed67016dad93dc7465023277ca3e1e21ee";
 
